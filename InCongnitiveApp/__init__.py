9d31427b08990b52b30a47f5b567bb47f7d8117c
themisK <tkoutsellis@epu.ntua.gr>
2022-12-02 01:26:08 +0200

    02Dec2022

--- InCongnitiveApp/__init__.py
+++ InCongnitiveApp/__init__.py
@@ -1,18 +1,19 @@
-"""The InCognitive App provides a GUI and the necessary backend code
-for Fuzzy Cognitive Map (FCM) applications. It provides the functionality
-of: a) stand alone FCM simulation and b) FCM combined with Monte
-Carlo Simulation (MCS), as well.
+#######################################################################
+""""The InCognitive App provides a GUI and backend code for Fuzzy
+Cognitive Map (FCM) simulations. It's functionalities are:
+a) stand alone FCM simulation and
+b) combination of FCM and Monte Carlo (MC) simulation.
 
-The stand alone FCM simulation provides the final state vector of the
-nodes, given certain input values.
+The 1st functionality provides the final nodes' state vector,
+given predifined input-node values.
 
-The FCM-MCS provides the distribution of the final node values,
-given that some or all of the input nodes or FCM parameters (e.g. weights)
-are random variables.
+The 2nd functionality, the FCM-MC simulation, provides the final
+node-values' distribution, given that some, or all, of the input
+nodes and/or the FCM parameters (e.g. weights) are random variables.
 
-The 'fcm_layout_dict' dictionary is the core InCognitive dictionary which
-contians all necessary info of the FCM layout. It allows all modules to
-communicate one another and it propagates through all functions/class methods
-from GUI all the way down to the backend code.
-
+The 'fcm_layout_dict' dictionary is the core InCognitive dictionary
+variable, which contains all necessary info of the FCM layout. It
+allows all modules to communicate one another: it is exchanged, as
+an argument, through all function/class methods, from GUI all the way
+down to the backend code.
 """

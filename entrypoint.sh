7305a49f956234670d4d525d20ee67c974bbb0e8
George Xexakis <gxexakis@gmail.com>
2023-11-21 18:32:22 +0200

    rename app file

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ if [ -z "${PREFIX}" ]; then
 else
     PREFIX_PARAM="--prefix ${PREFIX}";
 fi
-bokeh serve --port 5006 --address 0.0.0.0 --allow-websocket-origin=incognitivedev.paris-reinforce.epu.ntua.gr --show /InCongnitiveApp
+bokeh serve --port 5006 --address 0.0.0.0 --allow-websocket-origin=incognitivedev.paris-reinforce.epu.ntua.gr --show /InCognitiveApp
